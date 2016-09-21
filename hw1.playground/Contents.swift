834c1aa0dc979168b65b71420342f3cd242cf5d4
Elizabeth Lee <elizabeth.lee.hj@gmail.com>
2016-09-21 13:50:24 -0700

    finished hw1

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -17,16 +17,16 @@ class Words {
         self.wordA = wordA
         self.wordB = wordB
     }
-
-//: ### Are the values passed in to the **init** function and those set to the instance
-//: ### variables the same type? If not, why?
-
-
-//: [EXPLAIN YOUR ANSWER HERE]
-
-
-//: ## Q2: Variable Types and Function Types
-    func arePalindromes(_ words: [String]) -> Bool {
+    
+    //: ### Are the values passed in to the **init** function and those set to the instance
+    //: ### variables the same type? If not, why?
+    
+    
+    //: [No. String! implicitly unwraps options (cannot be nil). String? allows the possibility of nil.]
+    
+    
+    //: ## Q2: Variable Types and Function Types
+    class func arePalindromes(_ words: [String]) -> Bool {
         let reversedWords = words.map() {String($0.characters.reversed())}
         let numElements = words.count
         
@@ -35,20 +35,21 @@ class Words {
                 return false
             }
         }
+        return true
     }
-//: ### Why does the compiler dislike the **for loop**? Fix it.
-//: ### What else is wrong with this function? You may have to refer to (but **not**
-//: ### change) the code at the very bottom. Debug the function.
-
-
-//: [EXPLAIN YOUR ANSWER HERE]
-
-
-//: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
-        var countLetters : [Character : Int] //Line X
-        var lenA = self.wordA.characters.count
-        var lenB = self.wordB.characters.count
+    //: ### Why does the compiler dislike the **for loop**? Fix it.
+    //: ### What else is wrong with this function? You may have to refer to (but **not**
+    //: ### change) the code at the very bottom. Debug the function.
+    
+    
+    //: [EXPLAIN YOUR ANSWER HERE]
+    
+    
+    //: ## Q3: More Functions and Object Initialization
+    func isAnagram() -> Bool {
+        var countLetters = [Character : Int]() //Line X
+        let lenA = self.wordA.characters.count
+        let lenB = self.wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -75,21 +76,21 @@ class Words {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_, count) in countLetters {
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
-//: ### What is the problem with declaring **countLetters** as we do in **Line X**,
-//: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
-//: ### What else is wrong with this function? You may have to refer to (but **not**
-//: ### change) the code at the very bottom. Debug the function.
-
-
-//: [EXPLAIN YOUR ANSWER HERE]
+    //: ### What is the problem with declaring **countLetters** as we do in **Line X**,
+    //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
+    //: ### What else is wrong with this function? You may have to refer to (but **not**
+    //: ### change) the code at the very bottom. Debug the function.
+    
+    
+    //: [EXPLAIN YOUR ANSWER HERE]
     
     
 }
